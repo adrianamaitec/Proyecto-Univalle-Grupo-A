eba02a36034ebe71686afebefa0455b479d660d4
adrianamaitec <88515150+adrianamaitec@users.noreply.github.com>
2023-10-02 20:11:07 -0400

    Create DataMissingObject.tsx
    Se agrego en la carpeta DataTools un nuevo Archivo tsx para los datos de objetos perdidos

---
+++ src/app/DataTools/DataMissingObject.tsx
@@ -0,0 +1,105 @@
+const objetosPerdidos = [
+    {
+      id: 1,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 1"
+    },
+    {
+      id: 2,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 2"
+    },
+    {
+      id: 3,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 3"
+    },
+    {
+      id: 4,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 4"
+    },
+    {
+      id: 5,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 5"
+    },
+    {
+      id: 6,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 6"
+    },
+    {
+      id: 7,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 7"
+    },
+    {
+      id: 8,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 8"
+    },
+    {
+      id: 9,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 9"
+    },
+    {
+      id: 10,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 10"
+    },
+    {
+      id: 11,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 11"
+    },
+    {
+      id: 12,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 12"
+    },
+    {
+      id: 13,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 13"
+    },
+    {
+      id: 14,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 14"
+    },
+    {
+      id: 15,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 15"
+    },
+    {
+      id: 16,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 16"
+    },
+    {
+      id: 17,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 17"
+    },
+    {
+      id: 18,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 18"
+    },
+    {
+      id: 19,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 19"
+    },
+    {
+      id: 20,
+      img: "https://static.vecteezy.com/system/resources/previews/017/122/087/non_2x/school-backpack-supplies-free-vector.jpg",
+      nombre: "Objeto perdido 20"
+    }
+  ];
+  
+  export default objetosPerdidos;
+  
